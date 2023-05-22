59f561c66aeb3e53c5a85b9c5bcece86efb78688
Sebastian Velez <info.cbasdev@gmail.com>
2023-05-22 10:47:44 -0500

    :wrench: Configuration initial project

---
+++ src/main/java/app/auth/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package app.auth.controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+  @GetMapping("/login")
+  public String loginUser() {
+    return "Request Prueba";
+  }
+}
